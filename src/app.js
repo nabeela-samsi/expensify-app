aa97ea6eb6184e929c7b05b10d0e1a0cf7d3168f
Nabeela <nabeela.samsi.in@gmail.com>
2022-12-17 10:47:59 +0200

    reversed the changes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 //provider allows us to provide the store to all of the components that makes up the application
-import AppRouter from './routers/AppRouter';
-import store from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import selectExpenses from './selector/expenses';
-import 'normalize.css/normalize.css'; //this removes the browser styles, and considers the styles given by the user
-import './styles/styles.scss'
-import 'react-dates/lib/css/_datepicker.css';
+import AppRouter from "./routers/AppRouter";
+import store from "./store/configureStore";
+import "normalize.css/normalize.css"; //this removes the browser styles, and considers the styles given by the user
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
 
 const jsx = (
-    <Provider store = {store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('App'));
+ReactDOM.render(jsx, document.getElementById("App"));
